9f841508e9e2d5ff32286f2b69af7d44eebc5f52
Vivek Gautam <vivekbgautam@gmail.com>
2026-01-06 00:43:36 +0530

    Add files via upload

---
+++ Generic -Practice-program/program458.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+double Addition(double No1, double No2)
+{
+    double Ans;
+    Ans = No1 + No2;
+    return Ans; 
+}
+
+int main()
+{
+    double i = 0.0, j = 0.0;
+    double Ret = 0.0;
+
+    cout<<"Enter the first number : \n";
+    cin>>i;
+
+    cout<<"Enter the Second number : \n";
+    cin>>j;
+
+    Ret = Addition(i,j);
+    cout<<"Addition is : "<<Ret<<"\n";
+
+    return 0;
+}
