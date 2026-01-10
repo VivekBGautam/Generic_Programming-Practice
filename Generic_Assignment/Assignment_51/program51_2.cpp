f089e9063a5a03f248cfe281f7a9c440a67dc249
Vivek Gautam <vivekbgautam@gmail.com>
2026-01-10 23:52:01 +0530

    Add files via upload

---
+++ Generic_Assignment/Assignment_51/program51_2.cpp
@@ -0,0 +1,64 @@
+// Write a generic program to accept N values and count frequency of any specific value
+
+#include<iostream>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Namespace     : std
+// Description   : Standard C++ namespace which contains predefined classes,
+//                 objects and functions such as cout and cin
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Frequency
+// Description   : Generic function which accepts N values and Count frequency of any  specific value
+// Input         : T Value   | Value of any data type to be printed
+//                 int iSize | Number of times the value should be printed
+//                 T No      | Value of any data type to Count frequency
+// Output        : Return Count of given value
+// Author        : Vivek Bjauraj Gautam
+// Date          : 07-01-2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class T>
+T Frequency(T *Arr, int iSize, T No)
+{
+    int iCnt = 0;
+    int iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == No)
+        {
+            iCount++;
+        }
+    }
+
+    return iCount;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Description   : Entry point function of the program. Accepts input from user
+//                 and calls function 
+// Input         : Takes value and count from standard input
+// Output        : Displays the value on standard output
+// Author        : Vivek Bhauraj Gautam
+// Date          : 06-01-2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    char iValue1 = '\0';
+    int Ret = 0;
+    int Arr[] = {10,20,30,10,30,40,10,40,10};
+
+    Ret = Frequency(Arr,9, 10);
+    cout<<"frequency of "<<10<<" is "<<Ret<<"\n";
+
+    return 0;
+}
